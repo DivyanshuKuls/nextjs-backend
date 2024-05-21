dedd2e4d239cdb48171b188074edafdec5ed0aa3
Divyanshu Kulshreshtha <dk42221@gmail.com>
2024-05-22 00:37:31 +0530

    prisma and signup page added

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,3 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
 export async function GET() {
-    return Response.json ({username: "divyanshu", email: "divyanshu@yopmail.com"})
+  //database logic
+  return Response.json({ name: "divyanshu", email: "divyanshu@yopmail.com" });
+}
+
+export async function POST(req: NextRequest) {
+  // extract body
+  const body = await req.json();
+  console.log(body);
+
+  client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+  return NextResponse.json({
+    message: "You are logged in!",
+  });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function getUserDetails() {
   // await new Promise((r) => setTimeout(r, 5000))
-  const response = await axios.get("http://localhost:3000/api/user")
+  const response = await axios.get("http://localhost:3000/api/user");
   return response.data;
 }
 
@@ -12,15 +12,13 @@ export default async function Home() {
 
   return (
     <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userDetails?.name}
-                </div>
-                
-                {userDetails?.email}
-            </div>
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userDetails?.name}</div>
+
+          {userDetails?.email}
         </div>
+      </div>
     </div>
   );
 }
